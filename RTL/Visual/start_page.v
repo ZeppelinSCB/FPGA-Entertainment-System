7811c6640ef86f9bd455a50fe6ab32e1cd7c57b2
Sean <salmonseanz@gmail.com>
2024-12-03 22:53:38 +0800

    Start Page File Created

---
+++ RTL/Visual/start_page.v
@@ -0,0 +1,14 @@
+`include "../libs/define.vh"
+
+module start_page
+(
+    vga_clk,
+    sys_rst_n,
+    one_resistor_x,
+    two_resistors_x,
+    one_resistor_y,
+    two_resistors_y,
+    clk
+);
+
+endmodule
