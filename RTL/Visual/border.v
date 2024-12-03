6ed11e84974d0e34b314e444ae2a4b290325245d
Sean <salmonseanz@gmail.com>
2024-12-03 22:09:53 +0800

    Update the Border Logic

--- RTL/Visual/border.v
+++ RTL/Visual/border.v
@@ -4,25 +4,53 @@ module border
     sys_rst_n   ,
     pix_x       ,
     pix_y       ,
-    pix_data    
+    pix_data    ,
+    time_1s     ,
+    time_10s    ,
+    time_100s   ,
+    number_i    ,
+    number_x    ,
+    number_y    
 );
 
-input vga_clk  ;
-input sys_rst_n;
+input             vga_clk      ;
+input             sys_rst_n    ;
+input [ 3:0]      time_1s      ;  
+input [ 3:0]      time_10s     ;
+input [ 3:0]      time_100s    ;
 
-output pix_x   ;
-output pix_y   ;
-output pix_data;
+output         pix_x        ;
+output         pix_y        ;
+output         pix_data     ;
+output [3:0]   number_i     ;
+output [3:0]   number_x     ;
+output [3:0]   number_y     ;
 
+reg score_colour;
+
+parameter BORDER_COLOUR = 16'h5746;
+parameter TIME_H = VGA_HEIGHT - 1 - 16;
+parameter TIME_LEFT = ((VGA_WIDTH - 1)/2) - 24;
+parameter TIME_RIGHT = ((VGA_WIDTH - 1)/2) + 24;
+
+always@(*)//Colour of the score displayed
+    if(number_0 == 1)begin
+            score_colour = 16'hFFFF;
+        end
+        else begin
+            score_colour = 16'h0000;
+        end
 
 always@(posedge vga_clk or negedge sys_rst_n)//Edge of game area
-    if(sys_rst_n == 1'b0)
-        pix_data <= 16'd0;
-    else if((pix_x>=0)&&(pix_x<=16))||((pix_x<=639)&&(pix_x>=623))//Left and right sides fill with 16-pixel width green colour bar 
-        pix_data <= GREEN;
-    else if((pix_x>=16)&&(pix_y>=0)&&(pix_y<=16)||(pix_x>=16)&&(pix_y>=463)&&(pix_y<=479))//Up and down sides fill with 16-pixel width green colour bar 
-        pix_data <= GREEN;
-    else
-        pix_data <= 0;
+    if(sys_rst_n == 1'b0)begin
+        if((pix_y>=TIME_H)&&((pix_x>=TIME_LEFT)&&(pix_x<=TIME_RIGHT)))begin
+            pix_data = score_colour;
+        end
+        else begin
+            pix_data = BORDER_COLOUR;
+        end
+    end
 
+
+    
 endmodule
