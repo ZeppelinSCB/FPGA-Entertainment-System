8436acb21eec441f59702c866ea9ddcb0558203b
Sean <salmonseanz@gmail.com>
2024-12-06 16:38:33 +0800

    Fix Errors

--- RTL/Visual/page_end.v
+++ RTL/Visual/page_end.v
@@ -5,35 +5,38 @@ module page_end
 (
     vga_clk,
     sys_rst_n,
-    letter_req_1,
-    letter_req_2;
-    letter_req_colored_1;
-    letter_req_colored_2;
-    //number_req_time;
-    number_req_score;
-    //number_req_colored_time;
-    number_req_colored_score;
-    pix_x,
-    pix_y,
+    screen_x,
+    screen_y,
     pix_data,
     score,
-    game_state
+    game_over,
+    game_won
 );
 
 input wire             vga_clk      ;
 input wire             sys_rst_n    ;
 input wire             score        ;//Input from other module, should be converted to pixel information
+input wire             screen_x     ;
+input wire             screen_y     ;
+input wire             game_over    ;
+input wire             game_won     ; 
 
 output reg             pix_data     ;
-output reg             pix_x        ;
-output reg             pix_y        ;
 
-reg                    letter_req_1 ;
-reg                    letter_req_2 ;
-reg             letter_req_colored_1;
-reg             letter_req_colored_2;
-reg                    number_req   ;
+reg                    letter_req   ;
+reg               letter_req_colored;
+reg                 number_req_score;
 reg               number_req_colored;
+//reg                number_req_time;
+//reg        number_req_colored_time;
+reg                    game_state   ;
+reg                    stringIndex  ;
+reg                    arrayindex   ;
+reg                     letter_x    ;
+reg                     letter_y    ;
+reg                     number_x    ;
+reg                     number_y    ;
+
 
 parameter GAME_WON   = 1'b0         ;
 parameter GAME_LSE   = 1'b1         ;
@@ -69,17 +72,60 @@ parameter YELLOW = 16'hFFE0;
 
 
 always@(posedge vga_clk or negedge sys_rst_n)begin
-    if(game_state == GAME_LSE)begin
-        letter_req_1 = {24,14,20,26,3,8,4,3};//"YOU DIED"
-        letter_req_2 = {24,14,20,17,26,5,8,13,0,11,26,18,2,14,17,4};//"YOUR FINAL SCORE"
-        number_req_score = (score)%2;
-        case(letter_req_1)
-        1'b0:   letter_req_colored_1 = 16'h4040;//test* it should be BLACK(FFFF)
-        1'b1:   letter_req_colored_1 = WHITE;
-        default:letter_req_colored_1 = 16'h4040;//test* it should be BLACK(FFFF)
-        endcase
+    if((game_won == 1)&&(game_over != 1))
+        game_state <= 1'b1;//won
+    else if((game_won != 1)&&(game_over == 1))
+        game_state <= 1'b0;//lose, over
+    else
+        game_state <= 1'b0;
+
+    if(game_state == 1'b0)begin//lose, over
+        if(((pix_x >= Y_D_LEFT)&&(pix_x <= Y_D_RGHT))&&
+            ((pix_y >= Y_D_TOP)&&(pix_y <= Y_D_BTM)))begin
+                letter_req = {24,14,20,26,3,8,4,3};//"YOU DIED"
+                stringIndex <= (screen_x-Y_D_LEFT)/16
+                letter_i <= letter_req[arrayindex];
+                letter_x <= screen_x-Y_D_LEFT;
+                letter_y <= screen_y-Y_D_TOP;
+
+                case(letter_req)
+                    1'b0:   letter_req_colored = 16'h4040;//test* it should be BLACK(FFFF)
+                    1'b1:   letter_req_colored = WHITE;
+                    default:letter_req_colored = 16'h4040;//test* it should be BLACK(FFFF)
+                endcase
+
+                pix_data = letter_req_colored_1;
+            end
+        else if(((pix_x >= Y_F_S_LEFT)&&(pix_x <= Y_F_S_RGHT))&&
+                ((pix_y >= Y_F_S_TOP)&&(pix_y <= Y_F_S_BTM)))begin
+                letter_req = {24,14,20,17,26,5,8,13,0,11,26,18,2,14,17,4};//"YOUR FINAL SCORE"
+                stringIndex <= (screen_x-Y_F_S_LEFT)/16
+                letter_i <= letter_req[arrayindex];
+                letter_x <= screen_x-Y_F_S_LEFT;
+                letter_y <= screen_y-Y_F_S_TOP;
+
+                    case(letter_req)
+                        1'b0:   letter_req_colored = 16'h4040;//test* it should be BLACK(FFFF)
+                        1'b1:   letter_req_colored = WHITE;
+                        default:letter_req_colored = 16'h4040;//test* it should be BLACK(FFFF)
+                    endcase
+
+                pix_data = letter_req_colored_2;
+            end
+        else if(((pix_x >= SCORE_LEFT)&&(pix_x <= SCORE_RGHT))&&
+                ((pix_y >= SCORE_TOP)&&(pix_y <= SCORE_BTM)))begin
+                number_req_score = (score)%2;
+
+                case(number_req_score)
+                    1'b0:   number_req_colored_score = 16'h4040;//test* it should be BLACK(FFFF)
+                    1'b1:   number_req_colored_score = WHITE;
+                    default:number_req_colored_score = 16'h4040;//test* it should be BLACK(FFFF)
+                endcase
+
+                pix_data = number_req_colored_score;
+            end
         end
-    else if(game_state == GAME_WON)begin
+    else if(game_state == 1'b1)begin//won
         letter_req_1 = ({21,8,2,19,14,17,24,26,0,2,7,8,4,21,4,3}%2);//"VICTORY ACHIEVED;
         case(letter_req_1)
         1'b0:   letter_req_colored_1 = 16'h4040;//test* it should be BLACK(FFFF)
@@ -87,18 +133,6 @@ always@(posedge vga_clk or negedge sys_rst_n)begin
         default:letter_req_colored_1 = 16'h4040;//test* it should be BLACK(FFFF)
         endcase
         end
-    
-    case(letter_req_2)
-        1'b0:   letter_req_colored_2 = 16'h4040;//test* it should be BLACK(FFFF)
-        1'b1:   letter_req_colored_2 = WHITE;
-        default:letter_req_colored_2 = 16'h4040;//test* it should be BLACK(FFFF)
-    endcase
-
-    case(number_req_score)
-        1'b0:   number_req_colored_score = 16'h4040;//test* it should be BLACK(FFFF)
-        1'b1:   number_req_colored_score = WHITE;
-        default:number_req_colored_score = 16'h4040;//test* it should be BLACK(FFFF)
-    endcase
 
     //case(number_req_time)
     //    1'b0:   number_req_colored_time = 16'h4040;//test* it should be BLACK(FFFF)
@@ -107,12 +141,7 @@ always@(posedge vga_clk or negedge sys_rst_n)begin
     //endcase
 
     if(sys_rst_n == 1'b0)begin
-        if(((pix_x >= Y_D_LEFT)&&(pix_x <= Y_D_RGHT))&&((pix_y >= Y_D_TOP)&&(pix_y <= Y_D_BTM)))begin
-                pix_data = letter_req_colored_1;
-            end
-        else if(((pix_x >= Y_F_S_LEFT)&&(pix_x <= Y_F_S_RGHT))&&((pix_y >= Y_F_S_TOP)&&(pix_y <= Y_F_S_BTM)))begin
-                pix_data = letter_req_colored_2;
-            end
+        
         
         if(((pix_x >= V_A_LEFT)&&(pix_x <= V_A_RGHT))&&((pix_y >= V_A_TOP)&&(pix_y <= V_A_BTM)))begin
             pix_data = letter_req_colored_1;
@@ -123,4 +152,12 @@ always@(posedge vga_clk or negedge sys_rst_n)begin
             end
     end
 end
+
+letter letter_inst(
+.letter_req(letter_i),
+.letter_x(letter_x),
+.letter_y(letter_y),
+.letter_o(letter_o)
+);
+
 endmodule
--- RTL/Visual/page_start.v
+++ RTL/Visual/page_start.v
@@ -5,27 +5,22 @@ module page_start
 (
     vga_clk,
     sys_rst_n,
-    letter_req_1,
-    letter_req_2;
-    letter_req_colored_1;
-    letter_req_colored_2;
-    pix_x,
-    pix_y,
-    pix_data,
-    clk
+    screen_x,
+    screen_y,
+    pix_data
 );
 
 input wire             vga_clk      ;
 input wire             sys_rst_n    ;
+input wire             screen_x     ;
+input wire             screen_y     ;
 
 output reg             pix_data     ;
-output reg             pix_x        ;
-output reg             pix_y        ;
 
-reg                    letter_req_1 ;
-reg                    letter_req_2 ;
-reg             letter_req_colored_1;
-reg             letter_req_colored_2;
+reg                    letter_req   ;
+reg               letter_req_colored;
+reg                    stringIndex  ;
+reg                    arrayindex   ;
 
 parameter SNAKE_LEFT =(VGA_WIDTH/2) - 160 - 1;
 parameter SNAKE_RGHT =(VGA_WIDTH/2) + 160 - 1;
@@ -43,30 +38,48 @@ parameter SNAKE_COLOUR = 16'h5746;
 
 always@(posedge vga_clk or negedge sys_rst_n)begin
 
-    letter_req_1 = ({22,13,0,10,4}%4);//"SNAKE GAME"
-    letter_req_2 = {15,17,4,18,18,26,0,13,24,26,10,4,24};//"PRESS ANY KEY"
-
-    case(letter_req_1)
-        1'b0:   letter_req_colored_1 = 16'h4040;//test* it should be BLACK(FFFF)
-        1'b1:   letter_req_colored_1 = SNAKE_COLOUR;
-        default:letter_req_colored_1 = 16'h4040;//test* it should be BLACK(FFFF)
-    endcase
-
-    case(letter_req_2)
-        1'b0:   letter_req_colored_2 = 16'h4040;//test* it should be BLACK(FFFF)
-        1'b1:   letter_req_colored_2 = WHITE;
-        default:letter_req_colored_2 = 16'h4040;//test* it should be BLACK(FFFF)
-    endcase
-
     if(sys_rst_n == 1'b0)begin
-        if(((pix_x >= SNAKE_LEFT)&&(pix_x <= SNAKE_RGHT))&&((pix_y >= SNAKE_TOP)&&(pix_y <= SNAKE_BTM)))begin
-                pix_data = letter_req_colored_1;
+        if(((pix_x >= SNAKE_LEFT)&&(pix_x <= SNAKE_RGHT))&&
+            ((pix_y >= SNAKE_TOP)&&(pix_y <= SNAKE_BTM)))begin
+                letter_req = {22,13,0,10,4};//"SNAKE GAME"
+                stringIndex <= (screen_x-SNAKE_LEFT)/16
+                letter_i <= letter_req[arrayindex];
+                letter_x <= screen_x-SNAKE_LEFT;
+                letter_y <= screen_y-SNAKE_TOP;
+
+                case(letter_req)
+                    1'b0:   letter_req_colored = 16'h4040;//test* it should be BLACK(FFFF)
+                    1'b1:   letter_req_colored = SNAKE_COLOUR;
+                    default:letter_req_colored = 16'h4040;//test* it should be BLACK(FFFF)
+                endcase
+
+                pix_data = letter_req_colored;
             end
-        else if(((pix_x >= P_A_K_LEFT)&&(pix_x <= P_A_K_RGHT))&&((pix_y >= P_A_K_TOP)&&(pix_y <= P_A_K_BTM)))begin
-                pix_data = letter_req_colored_2;
+        else if(((pix_x >= P_A_K_LEFT)&&(pix_x <= P_A_K_RGHT))&&
+                ((pix_y >= P_A_K_TOP)&&(pix_y <= P_A_K_BTM)))begin
+                letter_req = {15,17,4,18,18,26,0,13,24,26,10,4,24};//"PRESS ANY KEY"
+                stringIndex <= (screen_x-P_A_K_LEFT)/16
+                letter_i <= letter_req[arrayindex];
+                letter_x <= screen_x-P_A_K_LEFT;
+                letter_y <= screen_y-P_A_K_TOP;
+
+                case(letter_req)
+                    1'b0:   letter_req_colored = 16'h4040;//test* it should be BLACK(FFFF)
+                    1'b1:   letter_req_colored = WHITE;
+                    default:letter_req_colored = 16'h4040;//test* it should be BLACK(FFFF)
+                endcase
+
+                pix_data = letter_req_colored;
             end
     end
 
-
 end
+
+letter letter_inst(
+.letter_req(letter_i),
+.letter_x(letter_x),
+.letter_y(letter_y),
+.letter_o(letter_o)
+);
+
 endmodule
