9cc742ec50e98759536a6f06d6ef89cb764d6a21
Sean <salmonseanz@gmail.com>
2024-12-06 16:38:46 +0800

    testbech

---
+++ Sim/tb_bintodeci.v
@@ -0,0 +1,18 @@
+`timescale 1ns/1ns
+module tb_bintodeci();
+
+reg [13:0] bin;
+wire [15:0] bcd;
+
+assign bcd = 0;
+
+always@(*)
+	bin = 234;
+
+
+bintodeci bintodeci_inst(
+    .bin(bin),
+    .bcd(bcd)
+);
+
+endmodule
